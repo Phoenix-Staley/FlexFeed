d4046c0ab9c7a84748a4735cc9d1440035e7b16a
phoenixstaley121@hotmail.com <phoenixstaley121@hotmail.com>
2025-04-07 13:40:59 -0700

    Update routes for views

--- routes/frontend/viewRoutes.js
+++ routes/frontend/viewRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve("views/index.html"));
+    res.status(200).sendFile(path.resolve("pages/index.html"));
 });
 
 module.exports = router;
