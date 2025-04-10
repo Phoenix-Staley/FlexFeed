e87fccf3f1fdc90f709b06040c4a03b68ea0d888
IvanD1061 <ivan.duenas@wsu.edu>
2025-04-10 16:46:47 -0700

    fixed the format of User.JS
    it was hard to read at first

--- models/User.js
+++ models/User.js
@@ -1,15 +1,20 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../connection');
+const sequelize = require('../config/connection');
 
 const User = sequelize.define('User', {
-  username: { type: DataTypes.STRING, unique: true, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  password_hash: { type: DataTypes.STRING, allowNull: false },
-  bio: { type: DataTypes.TEXT },
-  created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-}, {
-  tableName: 'users',
-  timestamps: false,
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 module.exports = User;
