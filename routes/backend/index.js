474684dafa46740147011bd565665f194527dcc9
Nihal Thomas <nihal.thomas@wsu.edu>
2025-04-14 11:05:48 -0700

    Something

--- routes/backend/index.js
+++ routes/backend/index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const app = express();
+
+const mediaRouter = require('/Users/nihalthomas/Library/CloudStorage/OneDrive-WashingtonStateUniversity(email.wsu.edu)/WSU/Senior/Semester 2/Web Dev/FlexFeed/FlexFeed/routes/backend/mediaRoutes.js'); // e.g., mediaRoutes.js
+app.use('/api/media', mediaRouter);
+
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
