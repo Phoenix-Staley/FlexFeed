4d11fd57b7198581891dee638620bf7ae3e0c4c5
phoenixstaley121@hotmail.com <phoenixstaley121@hotmail.com>
2025-04-14 22:07:58 -0700

    Adjust comment routes to new User model

--- routes/backend/commentRoutes.js
+++ routes/backend/commentRoutes.js
@@ -3,22 +3,25 @@ const { Post, User, Comment } = require('../../models');
 const with_auth = require('../../utils/auth');
 
 router.post('/', with_auth, async (req, res) => {
+    const poster = await User.findByPk(req.session.user_id);
+
     try {
         if (!req.body.postID || !req.body.content) {
             res.status(400).json({ message: 'Comments need a post ID and a body!' });
+            return;
         }
 
         const new_comment = await Comment.create({
             post_id: req.body.postID,
             content: req.body.content,
-            user_id: req.session.username,
+            username: poster.fullName,
             created_at: new Date(),
         });
 
         res.status(201).json(new_comment);
     } catch (err) {
         console.log(err);
-        res.status(400).json(err);
+        res.status(500).json("Error on the backend");
     }
 });
 
